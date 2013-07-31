8f9289a46c276584798d4a1c1ad1dcb9a419a98a
Steven Tan <steventangz@yahoo.ca>
2013-07-30 23:41:39 -0400

    k

--- src/com/ivywire/piratespeechflashcards/BeginnerActivity.java
+++ src/com/ivywire/piratespeechflashcards/BeginnerActivity.java
@@ -2,10 +2,15 @@ package com.ivywire.piratespeechflashcards;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.view.ViewPager;
+import android.view.GestureDetector;
 import android.view.Menu;
 
 public class BeginnerActivity extends Activity {
-
+	GestureDetector gesturedetector = null;
+	CardCursorPagerAdapter adapter;
+	ViewPager pager;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
