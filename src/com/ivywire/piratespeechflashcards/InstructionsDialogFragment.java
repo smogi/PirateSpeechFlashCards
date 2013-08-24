a40ce08a7debeb4d77a48e6a54dff4138cd309fc
Karan <karanbirr@hotmail.com>
2013-08-24 17:34:10 -0400

    stuff

---
+++ src/com/ivywire/piratespeechflashcards/InstructionsDialogFragment.java
@@ -0,0 +1,24 @@
+package com.ivywire.piratespeechflashcards;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+public class InstructionsDialogFragment extends DialogFragment{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        return dialog;
+    }
+}
