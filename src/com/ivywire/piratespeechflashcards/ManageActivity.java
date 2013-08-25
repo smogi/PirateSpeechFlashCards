06691d6cf7af7c2cae5aade5563fc3d81f75faa3
ivywireline <ivywireline@gmail.com>
2013-08-25 00:47:26 -0400

    a

--- src/com/ivywire/piratespeechflashcards/ManageActivity.java
+++ src/com/ivywire/piratespeechflashcards/ManageActivity.java
@@ -7,25 +7,44 @@ import android.os.Bundle;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
-
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+     
 public class ManageActivity extends FragmentActivity implements OnClickListener {
 	private Button removeNaughtyButton;
 	private String disabled;
 	private String naughty;
+	private PopupWindow popUp;
+	private LinearLayout layout;
+	private TextView tv;
+	private LayoutParams params;
+	private LinearLayout mainLayout;
+	private boolean click = true;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manage);
 		
+		popUp = new PopupWindow(this);
+		layout = new LinearLayout(this);
+		mainLayout = new LinearLayout(this);
+		tv = new TextView(this);
+		removeNaughtyButton = new Button(this);
 		removeNaughtyButton = (Button) findViewById(R.id.remove_naughty_button);
-		
 		removeNaughtyButton.setOnClickListener(this);
+		mainLayout.addView(removeNaughtyButton, params);
+        setContentView(mainLayout);
+		
+		
 	}
 
 	@Override
@@ -62,6 +81,19 @@ public class ManageActivity extends FragmentActivity implements OnClickListener 
 	public void onClick(View v) {
         switch(v.getId()){
             case R.id.remove_naughty_button:
+            	if (click) {
+            		popUp.showAtLocation(mainLayout, Gravity.BOTTOM, 10, 10);
+            		popUp.update(50, 50, 300, 80);
+            		click = false;
+            	}else{
+            		popUp.dismiss();
+            		click = true;
+            	}
+            	params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            	layout.setOrientation(LinearLayout.VERTICAL);
+            	tv.setText("All naughty cards are now disabled.");
+            	layout.addView(tv, params);
+            	popUp.setContentView(layout); 
             	turnNaughtyOff();
                 break;
             case R.id.naughty_password_button:
